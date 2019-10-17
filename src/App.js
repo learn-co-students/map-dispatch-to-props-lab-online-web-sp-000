9753143e284ed3979e4b6aaa2512fd300166181d
Adam Sarli <adam.sarli@gmail.com>
2019-10-17 14:12:14 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
+import { connect } from 'react-redux'
+import { addRestaurant } from './actions/restaurants'
 
 export class App extends Component {
   render() {
@@ -13,4 +15,14 @@ export class App extends Component {
   }
 };
 
-export default App;
+const mapStateToProps = (state) => state
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addRestaurant: (restaurant) => {
+      dispatch(addRestaurant(restaurant))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
