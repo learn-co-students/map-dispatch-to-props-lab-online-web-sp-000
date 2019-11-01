52462e07d791271ee58861f8e877ecb94faba7f9
Alan Krajina <alankrajina88@gmail.com>
2019-11-01 12:01:20 +0000

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -50,7 +50,15 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: (newRestaurant) => {
+      dispatch(addRestaurant(newRestaurant))
+    }
+  };
+};
+
+export default connect(null, mapDispatchToProps)(RestaurantInput);
 
 
-//connect this component by wrapping RestaurantInput below
-export default connect(null,{ addRestaurant })(RestaurantInput);
+//export default connect(null,{ addRestaurant })(RestaurantInput);
