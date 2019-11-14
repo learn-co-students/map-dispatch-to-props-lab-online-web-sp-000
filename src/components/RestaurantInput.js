e3b66ad40b4baa8318b11484f2ccdb8bfae1048a
fut0k0 <cccabell@icloud.com>
2019-11-14 13:05:22 -0800

    Passing lab

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,54 +1,65 @@
-import React, { Component } from 'react';
-import { addRestaurant } from '../actions/restaurants';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import { addRestaurant } from "../actions/restaurants";
 
-export class RestaurantInput extends Component {
+class RestaurantInput extends React.Component {
 
-  state = {
-    name: '',
-    location: ''
-  }
+	state = {
+		name: "",
+		location: ""
+	}
 
-  handleOnNameChange = event => {
-    this.setState({
-      name: event.target.value
-    });
-  }
+	handleOnNameChange = (event) => {
+		this.setState({
+			name: event.target.value
+		})
+	}
 
-  handleOnLocationChange = event => {
-    this.setState({
-      location: event.target.value
-    });
-  }
+	handleOnLocationChange = (event) => {
+		this.setState({
+			location: event.target.value
+		})
+	}
 
-  handleOnSubmit = event => {
-    event.preventDefault();
-    // add missing code
-  }
+	handleOnSubmit = (event) => {
+		event.preventDefault();
+		this.props.addRestaurant(this.state);
+		this.setState({
+			name: "",
+			location: ""
+		});
+	}
 
-  render() {
-    return(
-      <form onSubmit={(event) => this.handleOnSubmit(event)}>
-        <p>
-          <input
-            type="text"
-            onChange={(event) => this.handleOnNameChange(event)}
-            id="name"
-            placeholder="restaurant name" />
-        </p>
-        <p>
-          <input
-            type="text"
-            onChange={(event) => this.handleOnLocationChange(event)}
-            id="location"
-            placeholder="location" />
-        </p>
-        <input type="submit" />
-      </form>
-    );
-  }
-};
+	render() {
+		return(
+			<form onSubmit={this.handleOnSubmit}>
+				<p>
+					<input
+						type="text"
+						value={this.state.name}
+						onChange={this.handleOnNameChange}
+						id="name"
+						placeholder="restaurant name" />
+				</p>
+				<p>
+					<input
+						type="text"
+						value={this.state.location}
+						onChange={this.handleOnLocationChange}
+						id="location"
+						placeholder="location" />
+				</p>
+				<input type="submit" />
+			</form>
+		)
+	}
 
+}
 
-//connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+const mapDispatchToProps = (dispatch) => {
+	return {
+		addRestaurant: (restaurant) => dispatch(addRestaurant(restaurant))
+	}
+}
+
+export default connect(null, mapDispatchToProps)(RestaurantInput)
