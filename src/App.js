2afb28b619e336318c23a1d5cd61a07d6b4d13f5
Daniel Dawson <dannyldawson@gmail.com>
2019-12-11 14:47:37 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import RestaurantInput from './components/RestaurantInput';
-import Restaurants from './components/Restaurants';
+import React, { Component } from "react";
+import RestaurantInput from "./components/RestaurantInput";
+import Restaurants from "./components/Restaurants";
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
         <RestaurantInput />
+        <p>Holla</p>
         <Restaurants />
       </div>
     );
   }
-};
+}
 
 export default App;
