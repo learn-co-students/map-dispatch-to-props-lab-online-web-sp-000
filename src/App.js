521fe412ff4c1472a7e99a1b3094e3ef2600c501
axiswolf <karenvo@usc.edu>
2020-02-13 04:29:51 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
+// added files 
+import { connect } from 'react-redux';
+import { addRestaurant } from './actions/restaurants';
 
 export class App extends Component {
   render() {
