d08a9d694c173e654b58103dcafde30f1641a34c
github username <github email address>
2020-05-30 01:46:12 -0400

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -24,6 +24,8 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    //this.props.addRestaurant()                //WRONG
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -50,5 +52,25 @@ export class RestaurantInput extends Component {
 };
 
 
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    // addRestaurant: () => {                 //WRONG
+    addRestaurant: (newRestaurant) => {
+      //dispatch(addRestaurant())               //WRONG
+      dispatch(addRestaurant(newRestaurant))
+    }
+  }
+}
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, mapDispatchToProps)(RestaurantInput);
+//export default connect(mapDispatchToProps)(RestaurantInput);      //WRONG
+
+
+
+
+
+// Redux is already set up through index.js and the reducer manageRestaurants. Write a mapDispatchToProps() function that allows us to pass dispatched actions as props.
+//
+// Remember that mapDispatchToProps() is provided dispatch as an argument (passed in by connect when called), so we can wrap an imported action with dispatch within mapDispatchToProps(). Don't forget that the action provided in actions/restaurants.js is a function that must be called in order to return the action object.
