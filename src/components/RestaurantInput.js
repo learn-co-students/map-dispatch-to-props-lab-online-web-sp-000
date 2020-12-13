54113b8b728a55a58e16f23b9ae2c91b68b8159f
h-miiaah <haim.miiaah5@gmail.com>
2020-12-13 18:32:50 -0500

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,3 +1,83 @@
+// import React, { Component } from 'react';
+// import { addRestaurant } from '../actions/restaurants';
+// import { connect } from 'react-redux';
+
+// export class RestaurantInput extends Component {
+
+//   state = {
+//     name: '',
+//     location: ''
+//   }
+
+//   handleOnNameChange = event => {
+//     this.setState({
+//       name: event.target.value
+//     });
+//   }
+
+//   handleOnLocationChange = event => {
+//     this.setState({
+//       location: event.target.value
+//     });
+//   }
+
+//   handleOnSubmit = event => {
+//     event.preventDefault();
+//     // add missing code
+//     this.props.addRestaurant(this.state);
+//   }
+
+// //   render() {
+// //     return(
+// //       <form onSubmit={(event) => this.handleOnSubmit(event)}>
+// //         <p>
+// //           <input
+// //             type="text"
+// //             onChange={(event) => this.handleOnNameChange(event)}
+// //             id="name"
+// //             placeholder="restaurant name" />
+// //         </p>
+// //         <p>
+// //           <input
+// //             type="text"
+// //             onChange={(event) => this.handleOnLocationChange(event)}
+// //             id="location"
+// //             placeholder="location" />
+// //         </p>
+// //         <input type="submit" />
+// //       </form>
+// //     );
+// //   }
+// // };
+
+// render() {
+//   return(
+//     <form onSubmit={(event) => this.handleOnSubmit(event)}>
+//       <p>
+//         <input
+//           type="text"
+//           onChange={(event) => this.handleOnNameChange(event)}
+//           value={this.state.name}
+//           placeholder="restaurant name" />
+//       </p>
+//       <p>
+//         <input
+//           type="text"
+//           onChange={(event) => this.handleOnLocationChange(event)}
+//           value={this.state.location}
+//           placeholder="location" />
+//       </p>
+//       <input type="submit" />
+//     </form>
+//   );
+// }
+// };
+
+
+// //connect this component by wrapping RestaurantInput below
+// // export default RestaurantInput
+// export default connect(null, mapDispatchToProps)(RestaurantInput);
+
 import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
@@ -23,7 +103,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state);
   }
 
   render() {
@@ -33,14 +113,14 @@ export class RestaurantInput extends Component {
           <input
             type="text"
             onChange={(event) => this.handleOnNameChange(event)}
-            id="name"
+            value={this.state.name}
             placeholder="restaurant name" />
         </p>
         <p>
           <input
             type="text"
             onChange={(event) => this.handleOnLocationChange(event)}
-            id="location"
+            value={this.state.location}
             placeholder="location" />
         </p>
         <input type="submit" />
@@ -49,6 +129,17 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: (newRestaurant) => {
+      dispatch(addRestaurant(newRestaurant))
+    }
+  };
+};
 
-//connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+
+export default connect(null, mapDispatchToProps)(RestaurantInput);
+
+// Alternative form:
+
+// export default connect(null, { addRestaurant })(RestaurantInput);
