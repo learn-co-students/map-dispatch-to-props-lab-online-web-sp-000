9b0d42ebba57402470c95b3654b2d51b28f06a99
SVRourke <samuelvanrourke@gmail.com>
2021-03-02 20:44:20 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
 
@@ -12,5 +13,4 @@ export class App extends Component {
     );
   }
 };
-
 export default App;
