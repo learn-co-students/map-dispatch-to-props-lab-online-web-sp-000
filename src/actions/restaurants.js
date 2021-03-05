87a4970bc785ae7e8e9feeaa61c11c0cb137ae98
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 16:51:33 +0000

    Done.

--- src/actions/restaurants.js
+++ src/actions/restaurants.js
@@ -1,6 +1,6 @@
 export const addRestaurant = (restaurant) => {
-  return {
-    type: 'ADD_RESTAURANT',
-    restaurant
-  };
+    return {
+        type: 'ADD_RESTAURANT',
+        restaurant
+    };
 };
