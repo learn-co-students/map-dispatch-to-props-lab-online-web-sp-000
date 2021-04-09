e4ef6f7190d2b60925aebbc0b9da36a134024036
anpham148 <anpham148@gmail.com>
2021-04-09 11:32:10 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addRestaurant } from './actions/restaurants';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
 
 export class App extends Component {
+  
   render() {
     return (
       <div className="App">
@@ -12,5 +15,7 @@ export class App extends Component {
     );
   }
 };
+
+
 
 export default App;
