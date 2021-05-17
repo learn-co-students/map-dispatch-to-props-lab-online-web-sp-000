7908c9e2845bb8f8336345572752f215df48e937
Austin Rhoads <austinrrhoads@ymail.com>
2021-05-16 20:11:03 -0500

    i saw my reflection in a snow covered hill

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
+import {connect} from 'react-redux'
+
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
-        <RestaurantInput />
+        <RestaurantInput addRestaurant />
         <Restaurants />
       </div>
     );
   }
 };
 
-export default App;
+
+
+export default connect(state => ({restaurants: state.resaurants}))(App);
